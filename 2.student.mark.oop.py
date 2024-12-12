71cf68ccff1890b629a5a237d92a099ca4386de2
Vu Hoai Nam <156559580+Norl-Wu@users.noreply.github.com>
2024-12-12 14:59:31 +0700

    First student commit

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,24 +1,7 @@
-class methodManager:
-    @staticmethod
-    def addToList(obj, list):
-        list.append(obj)
-    
-    @staticmethod
-    def printList(list):
-        for obj in list:
-            print(obj)
-    
-    @staticmethod
-    def _inputQuantity():
-        return int(input("Enter the quantity you want: "))
-
-    @staticmethod
-    def InputQuantity():
-        return methodManager._inputQuantity()
 class Init:
     def __init__(self):
-        self.Name=input("Input student name: ")
-        self.ID=input("Input student ID: ")
+        self.Name=input("Input name: ")
+        self.ID=input("Input ID: ")
         
 class Student(Init):
     def __init__(self):
@@ -34,23 +17,41 @@ class Student(Init):
 class Course(Init):
     def __init__(self):
         super().__init__()
-        self.Mark={}
+        self._Mark={}
         
     def __str__(self):
         return  f"ID: {self.ID}, Name: {self.Name}"
     
     def InputMark(self, studentList:list):
         for student in studentList:
-            self.Mark[student.ID] = {
+            self._Mark[student.ID] = {
                 "Name": student.Name,
                 "Mark": float(input(f"Input mark for {student.Name}: "))
             }
     def PrintMark(self):
-        for key,value in self.Mark.items():
+        for key,value in self._Mark.items():
             print(f"Id: {key}, Name: {value['Name']}, Mark: {value['Mark']}")
+    
+def InputNumber(name):
+    return int(input(f"Input number {name}"))
 
-studentNumber=""
-courseNumber=""
+def SelectCourse(courseList, studentList)-> Course:
+    if len(studentList) == 0 or len(courseList) == 0:
+            print("Your student list or course list is empty")
+    else:    
+        PrintList(courseList)
+        selectedID = input("Input the course ID of the course you want to choose: ")
+        for course in courseList:
+            if selectedID == course.ID:
+                return course
+
+def PrintList(list):
+    for obj in list:
+        print(obj)
+
+
+studentNumber=0
+courseNumber=0
 studentList=[]
 courseList=[]
     
@@ -68,63 +69,45 @@ while True:
     action = int(input("Choose your action: "))
     
     if action == 1:
-        studentNumber=methodManager.InputQuantity()
+        studentNumber=InputNumber("of student:")
     elif action == 2:
         if studentNumber>0:
             for i in range(studentNumber):
+                print(f"Input infor for student {i+1}")
                 studentList.append(Student())
         else:
             print("You don't have number of student")
     elif action == 3:
-        courseNumber=methodManager.InputQuantity()
+        courseNumber=InputNumber("of course:")
     elif action == 4:
         if courseNumber>0:
+            print(f"Input infor for course {i+1}")
             for i in range(courseNumber):
                 courseList.append(Course())
         else:
             print("You don't have number of course")
     elif action == 5:
-        if len(studentList) == 0 or len(courseList) == 0:
-            print("Your student list or course list is empty")
-        else:    
-            methodManager.printList(courseList)
-            selectedID = input("Input the course ID of the course you want to update mark: ")
-            inList=0
-            for course in courseList:
-                if selectedID == course.ID:
-                    inList=1
-                    course.InputMark(studentList)
-            if inList==0:
-                print("Invalid ID")
-            
+        selectedCourse = SelectCourse(courseList,studentList)
+        if selectedCourse:
+            selectedCourse.InputMark(studentList)
+        else:
+            print("Course not exist")        
     elif action == 6:
         if len(courseList) == 0:
             print("Your course list is empty")
         else:
-            methodManager.printList(courseList)
+            PrintList(courseList)
     elif action == 7:
         if len(studentList) == 0:
             print("Your student list is empty")
         else:
-            methodManager.printList(studentList)
+            PrintList(studentList)
     elif action == 8:
-        if len(studentList) == 0 or len(courseList) == 0:
-            print("Your student list or course list is empty")
+        selectedCourse = SelectCourse(courseList,studentList)
+        if selectedCourse:
+            selectedCourse.PrintMark()
         else:
-            methodManager.printList(courseList)
-            selectedID = input("Input the course ID of the course you want to print mark: ")
-            inList=0
-            for course in courseList:
-                if selectedID == course.ID:
-                    if len(course.Mark)!=0:
-                        inList=1
-                        course.PrintMark()
-                        
-                    else:
-                        print("You haven't input mark for this course")
-                        break
-            if inList==0:
-                print("Invalid ID")
+            print("Course not exist") 
     elif action == 9:
         break
     else:
